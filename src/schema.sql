4afe1cb401b0729881deb2e707d59edde26136a8
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2019-10-19 16:25:36 +0200

    Add database constraints on referenced data

--- src/schema.sql
+++ src/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS clients (
 CREATE TABLE IF NOT EXISTS connections (
   server TEXT REFERENCES servers(name),
   client TEXT REFERENCES clients(name),
-  address inet NOT NULL,
+  address inet NOT NULL UNIQUE,
   PRIMARY KEY (server, client)
 );
 
@@ -43,4 +43,41 @@ CREATE TRIGGER notify_connections_changed
   ON connections
   EXECUTE FUNCTION notify_changes();
 
--- TODO add trigger for checking that the ip addresses in `connections` are valid.
+CREATE OR REPLACE FUNCTION check_integrity()
+  RETURNS trigger
+  AS $$
+    BEGIN
+      IF (SELECT COUNT(*) FROM connections c JOIN servers s on c.server = s.name WHERE NOT (c.address << subnet)) > 0
+      THEN
+        RAISE EXCEPTION 'Connection with address outside the server';
+      END IF;
+
+      IF (SELECT COUNT(*) FROM servers s1 JOIN servers s2 on s1.name != s2.name WHERE (s1.subnet <<= s2.subnet) OR (s1.subnet >>= s2.subnet)) > 0
+      THEN
+        RAISE EXCEPTION 'Overlapping server subnets';
+      END IF;
+
+      IF (SELECT COUNT(*) FROM connections c JOIN servers s on c.server = s.name WHERE c.address = s.address) > 0
+      THEN
+        RAISE EXCEPTION 'Client with server ip address';
+      END IF;
+      RETURN NULL;
+    END
+  $$
+  LANGUAGE PLPGSQL;
+
+DROP TRIGGER IF EXISTS check_integrity_servers ON public.servers;
+CREATE TRIGGER check_integrity_servers
+  AFTER INSERT OR UPDATE
+  ON servers
+  EXECUTE FUNCTION check_integrity();
+DROP TRIGGER IF EXISTS check_integrity_clients ON public.clients;
+CREATE TRIGGER check_integrity_clients
+  AFTER INSERT OR UPDATE
+  ON clients
+  EXECUTE FUNCTION check_integrity();
+DROP TRIGGER IF EXISTS check_integrity_connections ON public.connections;
+CREATE TRIGGER check_integrity_connections
+  AFTER INSERT OR UPDATE
+  ON connections
+  EXECUTE FUNCTION check_integrity();
